c2d4aaff020bf379711c49b9d5d77547d7dc5e1c
eddie <youcan689@gmail.com>
2021-04-12 18:37:49 +0800

    已經有訂單模型和遷移

---
+++ database/migrations/2021_04_12_064109_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->user_id('consumer');
+            //金額
+            $table->integer('cost');
+            //是否結帳
+            $table->boolean('isPay');
+            //結帳時間 強項 非強二四
+            $table->dateTime('payTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
